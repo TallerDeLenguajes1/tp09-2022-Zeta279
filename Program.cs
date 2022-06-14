a4baaf5a2f9e2c186a7951c4ab661a4412f01910
Zeta279 <equi27@live.com.ar>
2022-06-14 14:56:48 -0300

    Punto 1 finalizado

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+namespace TrabajoPractico9{
+    class Program{
+        static void Main(string[] args){
+            string path;
+            int cantidad;
+            List<Producto> listaProductos = new List<Producto>();
+
+            Console.Write("Escribe una ruta: ");
+            path = Console.ReadLine();
+
+            if(!File.Exists(path + @"/index.json")){
+                File.Create(path + @"/index.json").Close();
+            }
+
+            path += @"/index.json";
+
+            Console.Write("Ingresar la cantidad de productos a generar: ");
+            cantidad = Int32.Parse(Console.ReadLine());
+
+            for(int i = 0; i < cantidad; i++){
+                listaProductos.Add(new Producto());
+            }
+
+            string archivoJson = JsonSerializer.Serialize(listaProductos);
+            File.WriteAllText(path, archivoJson);
+
+            listaProductos = JsonSerializer.Deserialize<List<Producto>>(File.ReadAllText(path));
+
+            foreach(Producto prod in listaProductos){
+                Console.WriteLine(prod + "\n");
+            }
+        }
+    }
+}
